3452002dbcee1757fc9e70bf0e49078a7c4aa134
Daan van Berkel <daan.van.berkel@alliander.com>
2020-07-16 15:41:31 +0200

    accept path variable

--- backend/src/main/kotlin/nl/fifthpostulate/shortener/controller/Shortener.kt
+++ backend/src/main/kotlin/nl/fifthpostulate/shortener/controller/Shortener.kt
@@ -1,13 +1,14 @@
 package nl.fifthpostulate.shortener.controller
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletResponse
 
 @RestController
 class Shortener {
-    @GetMapping("/")
-    fun redirect(response: HttpServletResponse) {
-        response.sendRedirect("https://shrt.fifth-postulate.nl")
+    @GetMapping("/{short}")
+    fun redirect(@PathVariable short: String, response: HttpServletResponse) {
+        response.sendRedirect("https://shrt.fifth-postulate.nl/${short}")
     }
 }
