879506087f38fc6ad494dbf8febc7cb81632c9c0
Daan van Berkel <daan.van.berkel@alliander.com>
2020-07-22 19:41:27 +0200

    calculate the number of retrievals

--- backend/src/main/kotlin/nl/fifthpostulate/shortener/store/CouchDB.kt
+++ backend/src/main/kotlin/nl/fifthpostulate/shortener/store/CouchDB.kt
@@ -14,7 +14,7 @@ class CouchDB(val service: CouchDBService) : Store {
     }
 
     override fun store(dataSheet: DataSheet): Result<String, Unit> {
-        val event = EventInput(dataSheet)
+        val event = Created(dataSheet)
         return when (val result = service.save(event)) {
             is Success -> Success(result.data)
             is Failure -> Failure("could not store ${dataSheet}")
@@ -27,8 +27,8 @@ class CouchDB(val service: CouchDBService) : Store {
         return when(result) {
             is Success -> {
                 // TODO send retrieve event
-                val accessed = result.data.totalRows
-                val url = result.data.rows.filter { it.value?.type == "creation" }.firstOrNull()?.value?.url ?: "http://todo.com"
+                val url = result.data.rows.filter { it.value?.type == "created" }.firstOrNull()?.value?.url ?: "http://todo.com"
+                val accessed = result.data.rows.filter {it.value?.type == "retrieved"}.count()
                 Success(DataSheet(short, url, accessed))
             }
             is Failure -> Failure("no events found for ${short}")
@@ -43,12 +43,15 @@ data class Claim(val short: String) : Document() {
     override val attachments: Map<String, Attachment>? = null
 }
 
-class EventInput(@JsonUnwrapped dataSheet: DataSheet): Document() {
+sealed class EventInput(val type: String, @JsonUnwrapped val dataSheet: DataSheet): Document() {
     @JsonProperty("_id")
     override val id: String? = null
     override val revision: String? = null
     override val attachments: Map<String, Attachment>? = null
 }
+
+class Created(dataSheet: DataSheet): EventInput("created", dataSheet){}
+class Retrieved(dataSheet: DataSheet): EventInput("retrieved", dataSheet){}
 
 class Event() {
     var type: String? = null
