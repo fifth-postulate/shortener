bd3c4e9e8aab362ef3bdfada38b5559a3ad47401
Daan van Berkel <daan.van.berkel@alliander.com>
2020-07-22 14:42:43 +0200

    rename types of events

--- database/short/views/events/map.js
+++ database/short/views/events/map.js
@@ -1,8 +1,8 @@
 function(doc) {
-    if (doc.type === 'creation') {
+    if (doc.type === 'created') {
         emit(doc.short, { type: doc.type, url: doc.url });
     }
-    if (doc.type === 'retrieve') {
+    if (doc.type === 'retrieved') {
         emit(doc.short, { type: doc.type });
     }
 }
